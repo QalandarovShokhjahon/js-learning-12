f28eda3848dd390ef2ca980001a37a328c29e8ed
QalandarovShokhjahon <shokhjaxonqalandarov@gmail.com>
2025-11-28 15:57:42 +0500

    initial

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// Array → bir yoki bir nechta qiymatlarni index (0,1,2…) bo‘yicha tartiblab saqlaydigan ro‘yxat.
+
+let ranglar = ['qizil', 'yashil', 'malla']
+console.log(ranglar[2]);
